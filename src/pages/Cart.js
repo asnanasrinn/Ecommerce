25e0da2280461843ca26ef43e11e6c5059d31c9a
asnanasrinn <nasrinazeez2000@gmail.com>
2023-07-26 14:23:07 -0700

    checkout completed

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,9 +7,12 @@ import { Container, Row, Col } from "reactstrap";
 import { motion } from "framer-motion";
 import { cartActions } from "../redux/slices/cartSlice";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
+
+  const totalAmount= useSelector((state) => state.cart.totalAmount)
 
   return (
     <Helmet title="Cart">
@@ -32,12 +35,46 @@ const Cart = () => {
                     </tr>
                   </thead>
                   <tbody>
+                    
                     {cartItems.map((item, index) => (
-                      <Tr item={item} key={index} />
+                      // <Tr item={item} key={index} />
+                      <tr>
+                      <td>
+                        <img src={item.imgUrl} alt="" />
+                      </td>
+                      <td>{item.productName}</td>
+                      <td>${item.price}</td>
+                      <td>{item.quantity}px</td>
+                      <td>
+                        <motion.i 
+                          whileTap={{ scale: 1.2 }} 
+                          // onClick={deleteProduct}
+                          class="ri-delete-bin-line">
+                        </motion.i>
+                      </td>
+                    </tr>
                     ))}
                   </tbody>
                 </table>
               )}
+            </Col>
+
+            <Col lg="3">
+              <div>
+                <h6 className="d-flex align-items-center justify-content-between">
+                  Subtotal <span className="fs-4 fw-bold">${totalAmount}</span>
+                </h6>
+                
+              </div>
+              <p className="fs-6 mt-2">taxes and shipping will calculate in checkout</p>
+              <div>
+                <button className="buy_btn w-100 ">
+                  <Link to="/checkout">Checkout</Link>
+                </button>
+                <button className="buy_btn w-100 mt-3">
+                  <Link to="/shop">Continue Shopping</Link>
+                </button>
+              </div>
             </Col>
           </Row>
         </Container>
@@ -46,30 +83,30 @@ const Cart = () => {
   );
 };
 
-const Tr = ({ item }) => {
+// const Tr = ({ item }) => {
 
-  const dispatch = useDispatch()
+//   const dispatch = useDispatch()
 
-  const deleteProduct = () => {
-    dispatch(cartActions.deleteItem(item.id))
-  }
+//   const deleteProduct = () => {
+//     dispatch(cartActions.deleteItem(item.id))
+//   }
 
-  return;
-  <tr>
-    <td>
-      <img src={item.imgUrl} alt="" />
-    </td>
-    <td>{item.productName}</td>
-    <td>${item.price}</td>
-    <td>{item.quantity}px</td>
-    <td>
-      <motion.i 
-        whileTap={{ scale: 1.2 }} 
-        onClick={deleteProduct}
-        class="ri-delete-bin-line">
-      </motion.i>
-    </td>
-  </tr>;
-};
+//   return;
+//   <tr>
+//     <td>
+//       <img src={item.imgUrl} alt="" />
+//     </td>
+//     <td>{item.productName}</td>
+//     <td>${item.price}</td>
+//     <td>{item.quantity}px</td>
+//     <td>
+//       <motion.i 
+//         whileTap={{ scale: 1.2 }} 
+//         // onClick={deleteProduct}
+//         class="ri-delete-bin-line">
+//       </motion.i>
+//     </td>
+//   </tr>;
+// };
 
 export default Cart;
